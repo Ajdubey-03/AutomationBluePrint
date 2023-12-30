2fd1c9f53535a307571229e4160e846c87cabc4e
Ajay Dubey <ajdubey.qa@gmail.com>
2023-12-30 12:13:57 +0530

    if else basic

--- src/basic_04_17Dec/Lab050.java
+++ src/basic_04_17Dec/Lab050.java
@@ -1,10 +1,48 @@
 package basic_04_17Dec;
 
+import java.util.Scanner;
+
 public class Lab050 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// if else Statement yes,no ,true,false
+		
+//		if(true) 
+//		{
+//			System.out.println("True");
+//		}
+//		else 
+//		{
+//			System.out.println("false");
+//		}
+		
+		
+		if(true)
+		{
+			System.out.println("only true, No Else conditions");
+		}
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input here");
+		int num =sc.nextInt();
+		if(num>20)
+		{
+			System.out.println("num> 20");
+		}
+		else if(num>10)
+		{
+			System.out.println("num > 10");
+		}
+		else 
+		{
+		System.out.println("lesser value");
+		}
 
+		
+		sc.close();  //will closed the Scanner class here
 	}
+	
 
 }
