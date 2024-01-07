d1183c7062c4610eadd461676d3b3515e7e9ab5e
Ajay Dubey <ajdubey.qa@gmail.com>
2024-01-07 13:44:03 +0530

    String methods and Compare String values and refrecnde

---
+++ src/oops_06_jan_2024/basic/Lab0073.java
@@ -0,0 +1,28 @@
+package oops_06_jan_2024.basic;
+
+public class Lab0073 {
+
+	public static void main(String[] args) {
+		
+		String name = "Akshay";
+		System.out.println(name.concat(" Kumar")); // after concat it is creating another string
+		System.out.println(name + " kumaarr");
+		String name1=name.concat(" Dumeyy");
+		System.out.println(name1);
+		System.out.println(name.contains("a"));
+		System.out.println(name);  // Still will be same , because string is immutable 
+		String st = new String("Akshay");
+		System.out.println(st ==name);  // Expected false due to st is created i heap area or ob area, Reference is not same for boath
+		System.out.println(st.equals(name));  // Checking values only not reference , Expected true
+		
+
+		String Expected_pass = "password@123";
+		String Actual_pass ="PassworD@123";
+		if(Actual_pass.equalsIgnoreCase(Expected_pass)) {
+			System.out.println("Yes, Accepted");
+		}
+		
+		
+	}
+
+}
